ca08e1792a7efa699b26bfa72d357d74ded16198
André Teixeira <andre-x8@hotmail.com>
2024-02-10 15:50:00 -0300

    limpeza

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -3,7 +3,7 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ProductEntity } from './entities/product.entity';
-import { Repository, getRepository } from 'typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ProductService {
