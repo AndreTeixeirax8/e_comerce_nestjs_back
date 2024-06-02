f84a2d146acff46f9a21906a85d3c43575024df7
André Teixeira <andre-x8@hotmail.com>
2024-06-02 14:44:15 -0300

    adicionado status na criacao do atendimento

--- src/atendimento/repositories/atendimento.repository.ts
+++ src/atendimento/repositories/atendimento.repository.ts
@@ -1,30 +1,28 @@
-import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { AtendimentoEntity } from "src/atendimento/entities";
-import { Repository } from "typeorm";
-import { EditaAtendimentoDto,CriaAtendimentoDto } from "src/atendimento/dtos";
-import { IAtendimentoRepository } from "src/atendimento/interfaces";
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AtendimentoEntity } from 'src/atendimento/entities';
+import { Repository } from 'typeorm';
+import { EditaAtendimentoDto, CriaAtendimentoDto } from 'src/atendimento/dtos';
+import { IAtendimentoRepository } from 'src/atendimento/interfaces';
+import { StatusEnum } from 'src/atendimento/enums';
 
 @Injectable()
-export class AtendimentoRepository implements IAtendimentoRepository{
-    constructor(
-        @InjectRepository(AtendimentoEntity)
-        private atendimentoRepository: Repository<AtendimentoEntity>,
-    ){}
+export class AtendimentoRepository implements IAtendimentoRepository {
+  constructor(
+    @InjectRepository(AtendimentoEntity)
+    private atendimentoRepository: Repository<AtendimentoEntity>,
+  ) {}
 
-     criaUmRegistro(
-        data: CriaAtendimentoDto,
-      ): Promise<CriaAtendimentoDto> {
-        return this.atendimentoRepository.save(data);
-      }
+  criaUmRegistro(data: CriaAtendimentoDto): Promise<CriaAtendimentoDto> {
+    data.status = StatusEnum.ABERTO;
+    return this.atendimentoRepository.save(data);
+  }
 
-       buscaTodos(): Promise<CriaAtendimentoDto[]> {
-        return this.atendimentoRepository.find();
-      }
+  buscaTodos(): Promise<CriaAtendimentoDto[]> {
+    return this.atendimentoRepository.find();
+  }
 
-      editaUmRegistro(
-        data: EditaAtendimentoDto,
-      ): Promise<EditaAtendimentoDto> {
-        return this.atendimentoRepository.save(data);
-      }
-    }
+  editaUmRegistro(data: EditaAtendimentoDto): Promise<EditaAtendimentoDto> {
+    return this.atendimentoRepository.save(data);
+  }
+}
