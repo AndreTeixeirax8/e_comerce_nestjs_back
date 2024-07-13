e55f651405c17aa116e2aa899feae826f0ecbd7f
André Teixeira <andre-x8@hotmail.com>
2024-07-13 19:50:03 -0300

    teste de ajuste do main.ts vercel

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+//remover esse comentario para rodar localmente
+/*
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -7,4 +9,32 @@ async function bootstrap() {
   await app.listen(3000);
   console.log('Servidor rodado na porta 3000');
 }
-bootstrap();
+bootstrap();*/
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import serverlessExpress from '@vendia/serverless-express';
+import { Callback, Context, Handler } from 'aws-lambda';
+
+let server: Handler;
+
+async function bootstrap(): Promise<Handler> {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  await app.init();
+
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: expressApp });
+}
+
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  server = server ?? (await bootstrap());
+  return server(event, context, callback);
+};
+
+// Esta linha não é necessária em um ambiente serverless
+// bootstrap();*/
