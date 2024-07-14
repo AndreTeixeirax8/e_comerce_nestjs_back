02c187e97c8efebfae0453d6209ceac3e4decfea
André Teixeira <andre-x8@hotmail.com>
2024-07-14 15:54:08 -0300

    ajuste do ts.config

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,33 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { UsersService } from 'src/users/users.service';
-import * as bcrypt from 'bcryptjs'
+import { UsersService } from '../users/users.service';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
-    constructor(
-        private userService: UsersService,
-        private jwtService: JwtService
-        ){}
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
-    async validateUser(username:string,pass:string){
-
-        const user = await this.userService.findOne(username)
-        let value = bcrypt.compareSync(pass,user.password)
-        if(user && value ){
-            const {password,...result} = user
-            return result
-        }
-
-       
-        return null
-
+  async validateUser(username: string, pass: string) {
+    const user = await this.userService.findOne(username);
+    let value = bcrypt.compareSync(pass, user.password);
+    if (user && value) {
+      const { password, ...result } = user;
+      return result;
     }
 
-    async login(user:any){
-        const payload = {username:user.username,sub:user.id}
-        return{
-            access_token: this.jwtService.sign(payload)
-        }
-    }
+    return null;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
 }
