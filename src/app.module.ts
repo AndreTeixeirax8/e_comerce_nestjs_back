2c176f2a8dd8e8a37018de49346af929c0bb06cb
André Teixeira <andre-x8@hotmail.com>
2024-07-14 16:01:24 -0300

    ajuste do ts.config3

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,21 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ProductModule } from 'src/product';
-import { ProductEntity } from 'src/product/entities';
+
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { UserEntity } from 'src/users/entities';
+
 import { APP_GUARD } from '@nestjs/core';
-import { RolesGuard } from 'src/auth/roles.guard';
-import { ClienteEntity } from 'src/cliente/entities';
-import { ClienteModule } from 'src/cliente';
-import { AtendimentoModule } from 'src/atendimento';
-import { AtendimentoEntity } from 'src/atendimento/entities';
-import { OrigemAtendimentoEntity } from 'src/origem_atendimento/entities';
-import { OrigemAtendimentoModule } from 'src/origem_atendimento';
-import { TipoServicoEntity } from 'src/tipo_servico/entities';
-import { TipoServicoModule } from 'src/tipo_servico/tipo-servico.module';
+import { ProductEntity } from './product/entities';
+import { UserEntity } from './users/entities';
+import { ClienteEntity } from './cliente/entities';
+import { AtendimentoEntity } from './atendimento/entities';
+import { OrigemAtendimentoEntity } from './origem_atendimento/entities';
+import { TipoServicoEntity } from './tipo_servico/entities';
+import { ProductModule } from './product';
+import { ClienteModule } from './cliente';
+import { AtendimentoModule } from './atendimento';
+import { OrigemAtendimentoModule } from './origem_atendimento';
+import { TipoServicoModule } from './tipo_servico';
+import { RolesGuard } from './auth';
 
 @Module({
   imports: [
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,20 +1,20 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from 'src/users/users.module';
+
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
-import { 
-  JwtStrategy,
-  jwtConstants,
-  LocalStrategy,
-  AuthService,
- } from 'src/auth';
+import { JwtStrategy, jwtConstants, LocalStrategy, AuthService } from '../auth';
+import { UsersModule } from '../users';
 
 @Module({
-  imports:[UsersModule,PassportModule,JwtModule.register({
-    secret:jwtConstants.secret,
-    signOptions:{expiresIn:'14d'}
-  })],
-  providers: [AuthService,LocalStrategy,JwtStrategy],
-  exports:[AuthService]
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '14d' },
+    }),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
