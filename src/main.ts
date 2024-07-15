89b4677142488290f9634862aa921e87f3282a52
André Teixeira <andre-x8@hotmail.com>
2024-07-15 18:44:46 -0300

    ajuste para rodar local

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 //remover esse comentario para rodar localmente
-/*
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -9,27 +9,4 @@ async function bootstrap() {
   await app.listen(3000);
   console.log('Servidor rodado na porta 3000');
 }
-bootstrap();*/
-
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.init();
-
-  return app.getHttpAdapter().getInstance();
-}
-
-let app;
-
-export default async function handler(req, res) {
-  if (!app) {
-    app = await bootstrap();
-  }
-  app(req, res);
-}
-
-// Esta linha não é necessária em um ambiente serverless
-// bootstrap();*/
+bootstrap();
